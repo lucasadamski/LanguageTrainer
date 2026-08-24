e91d6aa2c79f980ee8ac17493581a65ebeddd5a7
Lucas A <lucasadams@protonmail.com>
2026-08-24 22:21:21 +0100

    Tests passed

--- src/features/gamePlayer.js
+++ src/features/gamePlayer.js
@@ -1,14 +1,30 @@
-function startNewGame() {
+let wordBank = [{word: '', definition: ''}]; 
+let iterator; 
+let length; 
+let gameOver = false;
 
+function startNewGame(collection) {
+    wordBank = collection;
+    iterator = 0;
+    length = collection.length;
 }
 
 function getWordGame() {
-
+    if(gameOver) return null;
+    if(wordBank[iterator] != null) return wordBank[iterator].word;
+    return null;
 }
 
-function provideAnswerForGame() {
-    
+function provideAnswerForGame(answer) {
+    if(gameOver) return null;
+    let result = false;
+    if(answer === null || answer === undefined) result = false;
+    if (answer === wordBank[iterator].definition) result = true;
+    if(iterator < length) 
+        iterator++;
+    else 
+        gameOver = true;
+    return result;
 }
-
 
 export { startNewGame, getWordGame, provideAnswerForGame };
