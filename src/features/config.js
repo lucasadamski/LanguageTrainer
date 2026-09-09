c7f6e1fd8d31e85f4e737183eec0c1e6d6ceb825
Lucas A <lucasadams@protonmail.com>
2026-09-09 16:57:44 +0100

    new config file

---
+++ src/features/config.js
@@ -0,0 +1,20 @@
+// Sounds
+const OK_ANSWER_SOUND_PATH = '../../media/good.wav';
+const BAD_ANSWER_SOUND_PATH = '../../media/bad.wav';
+const FIRST_BAD_ANSWER_SOUND_PATH = '../../media/cs-sounds/firstblood.wav';
+const FIRST_OK_ANSWER_SOUND_PATH = '../../media/cs-sounds/headshot.wav';
+const FIVE_OK_ANSWERS_SOUND_PATH = '../../media/cs-sounds/mosterkill.wav';
+const FIVE_BAD_ANSWERS_SOUND_PATH = '../../media/cs-sounds/humiliation.wav';
+const NEW_GAME_SOUND_PATH = '../../media/cs-sounds/prepare.wav';
+
+// Video
+const VIDEO_PATH = '../../media/damn.mp4';
+
+
+// Settings
+
+
+export { OK_ANSWER_SOUND_PATH, BAD_ANSWER_SOUND_PATH, 
+    FIRST_BAD_ANSWER_SOUND_PATH, FIRST_OK_ANSWER_SOUND_PATH, 
+    FIVE_OK_ANSWERS_SOUND_PATH, FIVE_BAD_ANSWERS_SOUND_PATH, 
+    NEW_GAME_SOUND_PATH, VIDEO_PATH };
