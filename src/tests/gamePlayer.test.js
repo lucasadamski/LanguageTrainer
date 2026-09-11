1dcbe2f4d85359e732f70d23bad3121704b9c8db
Lucas A <lucasadams@protonmail.com>
2026-09-11 12:39:09 +0100

    Failing unit tests

--- src/tests/gamePlayer.test.js
+++ src/tests/gamePlayer.test.js
@@ -28,8 +28,44 @@ test('try to get second word from one word collection, should return null', () =
   expect(word).toBe(null);
 });
 
+test('definitions separated by comma, provided good answer no 2, accepts one of them', () => {
+  // Arrange
+  let testCollection = [ {word: 'testWord', definition: 'testDef,testDef2, testDef3' } ];
+  startNewGame(testCollection);
+  let question = getWordGame();
+  let answer = 'testDef2';
+  
+  // Act
+  let response = provideUserInputToGameEngine(answer);
+  
+  // Assert
+  expect(response).toBe(true);
+});
 
+test('definitions separated by comma, provided good answer no 3, accepts one of them', () => {
+  // Arrange
+  let testCollection = [ {word: 'testWord', definition: 'testDef,testDef2, testDef3' } ];
+  startNewGame(testCollection);
+  let question = getWordGame();
+  let answer = 'testDef3';
+  
+  // Act
+  let response = provideUserInputToGameEngine(answer);
+  
+  // Assert
+  expect(response).toBe(true);
+});
 
-
-
-
+test('definitions separated by comma, provided bad answer, rejects it', () => {
+  // Arrange
+  let testCollection = [ {word: 'testWord', definition: 'testDef,testDef2, testDef3' } ];
+  startNewGame(testCollection);
+  let question = getWordGame();
+  let answer = 'badAnswer';
+  
+  // Act
+  let response = provideUserInputToGameEngine(answer);
+  
+  // Assert
+  expect(response).toBe(false);
+});
